8b77d8ae6adcaf500785e4cbb35f0c4e521e44c0
Debapriya Sen <desen@akamai.com>
2022-06-21 10:46:21 +0530

    improved search 2d matrix

---
+++ topics/leetcode_150/koko_eating_banana.py
@@ -0,0 +1,20 @@
+class Solution:
+
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        if h == len(piles):
+            return max(piles)
+        
+        low = 1
+        high = max(piles)
+        res = high
+        while low <= high:
+            mid = int(low + (high - low)/2)
+            hours = 0
+            for item in piles:
+                hours += math.ceil(item/mid)
+            if hours <= h:
+                res = min(res, mid)
+                high = mid - 1
+            else:
+                low = mid + 1
+        return res
