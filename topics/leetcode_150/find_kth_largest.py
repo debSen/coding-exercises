7f0bf8aa9126938356a0b3478351a8d38674ce9b
Debapriya Sen <desen@akamai.com>
2022-06-27 20:25:04 +0530

    find kth largest

---
+++ topics/leetcode_150/find_kth_largest.py
@@ -0,0 +1,9 @@
+import heapq
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        minheap = nums
+        heapq.heapify(minheap)
+        while len(minheap) > k:
+            heapq.heappop(minheap)
+        return minheap[0]
