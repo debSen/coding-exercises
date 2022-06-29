afcd2afe06d7bf121f4c5c99320c2659eeaf1d47
Debapriya Sen <desen@akamai.com>
2022-06-29 18:50:28 +0530

    same tree

--- topics/leetcode_150/same_tree.py
+++ topics/leetcode_150/same_tree.py
@@ -1,15 +1,7 @@
 class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if p is None and q is not None:
-            return False
-        if p is not None and q is None:
-            return False
-        if p is None and q is None:
-            return True
-        if p.val != q.val:
-            return False
-        
-
-        
-        return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q: return True
+        if p and q and p.val == q.val: 
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else: return False
         
